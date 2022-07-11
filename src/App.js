4a0a91a3a88e492f84d987f92fa000df673bfa8b
unknown <antonykibet03@gmail.com>
2022-07-11 09:41:16 +0300

    layout and folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Clients from './pages/Clients';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="bg-blue-200">
-    <h1 className=''>Hello</h1>
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/clients' element={<Clients/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Layout({ children }) {
+  return (
+    <div>
+
+        <div className="content">
+            {children}
+        </div>
+      <div className="footer">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
