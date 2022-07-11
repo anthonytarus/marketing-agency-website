f5d1dcca26a337f92b2058f32be9afae5581bedc
unknown <antonykibet03@gmail.com>
2022-07-11 10:10:47 +0300

    navigation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Layout({ children }) {
+  const menuItems = [
+    {
+      title: "Home",
+      path: "/",
+      icon: "",
+    },
+    {
+      title: "About",
+      path: "/about",
+      icon: "",
+    },
+    {
+      title: "Clients",
+      path: "/clients",
+      icon: "",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+      icon: "",
+    },
+  ];
   return (
     <div>
+      <div className="">{children}</div>
 
-        <div className="content">
-            {children}
+      <div className="fixed bottom-10 left-0 right-0 ">
+        <div className="flex w-full justify-center">
+          {menuItems.map((item, index) => (
+            <div
+              className={` bg-primary py-5 px-20 ${
+                index === 0 && "rounded-l"
+              } ${index === menuItems.length - 1 && "rounded-r"}`}
+            >
+              <Link to={`${item.path}`} className="text-secondary text-xl">
+                {item.title}
+              </Link>
+            </div>
+          ))}
         </div>
-      <div className="footer">
-        <h1>Footer</h1>
       </div>
     </div>
   );
